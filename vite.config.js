137e621bbd3f1490bd363daa0456b69b265e013c
Mariano García <marianogarciagz@gmail.com>
2024-01-21 00:31:08 -0600

    Fix base link

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "node:path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://marianogarciagz.github.io",
+  base: "https://marianogarciagz.github.io/",
   resolve: {
     alias: {
       "@img": path.resolve(__dirname, "./src/assets/img"),
