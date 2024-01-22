1413446d4f11b5a8c42a7cb023592892170c088b
Mariano García <marianogarciagz@gmail.com>
2024-01-22 17:36:23 -0600

    Fix font sizes footer

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -14,42 +14,42 @@ const Footer = () => {
           <div className="col-12 col-md-6 mb-4 mb-md-0">
             <h3 className="footer__h3 h3 mb-4">@MarianoGarciaGz</h3>
             <SocialLink
-              className="mb-2"
+              className="mb-2 fs-6"
               icon="github"
               href="https://github.com/MarianoGarciaGz"
             >
               Github
             </SocialLink>
             <SocialLink
-              className="mb-2"
+              className="mb-2 fs-6"
               icon="instagram"
               href="https://www.instagram.com/marianogarciagz"
             >
               instagram
             </SocialLink>
             <SocialLink
-              className="mb-2"
+              className="mb-2 fs-6"
               icon="linkedin"
               href="https://www.linkedin.com/in/marianogarciagz/"
             >
               linkedin
             </SocialLink>
             <SocialLink
-              className="mb-2"
+              className="mb-2 fs-6"
               icon="youtube"
               href="https://www.youtube.com/@marianogarciagz"
             >
               youtube
             </SocialLink>
             <SocialLink
-              className="mb-2"
+              className="mb-2 fs-6"
               icon="cloud-fill"
               href="https://soundcloud.com/dowdyprogrammer"
             >
               soundcloud
             </SocialLink>
             <SocialLink
-              className="mb-2"
+              className="mb-2 fs-6"
               icon="spotify"
               href="https://open.spotify.com/intl-es/artist/2k5EgJml0LNzElMQpj0QOW?si=Mm2WzVEKRhquHSztP4HG3w"
             >
@@ -62,7 +62,7 @@ const Footer = () => {
             <ul className="header-ul navbar-nav text-center ms-auto">
               {menu.map(({ id, href, title, icon }) => (
                 <SocialLink
-                  className="mb-2 text-start"
+                  className="mb-2 text-start fs-6"
                   href={href}
                   target="_self"
                   icon={icon}
