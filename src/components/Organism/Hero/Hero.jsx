30297669d0a8ea5f03ca00529875aee8f97b5b70
Mariano Garcia <marianogarciagz@gmail.com>
2024-06-05 20:17:30 -0600

    :sparkles: Feat: Add better Hero Design

--- src/components/Organism/Hero/Hero.jsx
+++ src/components/Organism/Hero/Hero.jsx
@@ -1,69 +1,58 @@
 import React from "react";
-import mariano from "@img/mariano.png";
+import mariano from "@img/mariano-photo.png";
+import ball from "@img/svg/blob.svg";
 
 import "./Hero.scss";
 
 const Hero = () => {
   return (
-    <section className="hero container-fluid d-flex align-items-center">
-      <div className="row mx-auto">
-        <div
-          className="col-12 col-lg-6 hero-div d-flex align-items-center"
-          data-aos="fade-right"
-          data-aos-duration="500"
-          data-aos-once="true"
-        >
-          <h1 className="hero-title h1 text-center text-lg-start display-1 mx-auto mx-lg-0">
-            Full-stack{" "}
-            <strike
-              className="hero-strike lh-lg"
-              data-aos="fade-up"
-              data-aos-delay="300"
-              data-aos-once="true"
-              data-aos-anchor-placement="bottom-bottom"
-            >
-              developer
-            </strike>{" "}
-            <br />
-            <em
-              className="hero-em"
-              data-aos="zoom-out-right"
-              data-aos-delay="500"
-              data-aos-once="true"
-            >
-              &
-            </em>{" "}
-            <strong
-              className="hero-strong"
-              data-aos="zoom-in"
-              data-aos-delay="500"
-              data-aos-once="true"
-            >
-              music producer
-            </strong>
-          </h1>
-        </div>
-        <div
-          className="col-12 col-lg-6 hero-back-img d-flex align-items-center"
-          data-aos="fade-left"
-          data-aos-delay="0"
-          data-aos-once="true"
-        >
-          <div
-            className=""
+    <section className="hero">
+      {/* Ball background */}
+      <div className="hero__photo hero__photo--ball position-absolute d-flex justify-content-center align-items-center vh-100 z-0">
+        <img className="hero__img-ball w-50" src={ball} alt="ball" />
+      </div>
+
+      {/* Text */}
+      <div
+        className="hero__text-container position-absolute d-flex w-100 h-100 justify-content-center align-items-center z-1"
+        data-aos="fade-right"
+        data-aos-duration="500"
+        data-aos-once="true"
+      >
+        <h1 className="hero__title h1 text-center align-self-center display-1">
+          Full-stack{" "}
+          <strike
+            className="hero__strike lh-lg"
+            data-aos="fade-up"
+            data-aos-delay="300"
+            data-aos-once="true"
             data-aos-anchor-placement="bottom-bottom"
-            data-aos="fade-down"
+          >
+            developer
+          </strike>{" "}
+          <br />
+          <em
+            className="hero__em"
+            data-aos="zoom-out-right"
             data-aos-delay="500"
-            data-aos-duration="500"
             data-aos-once="true"
           >
-            <img
-              className="hero-front-img align-self-center"
-              src={mariano}
-              alt=""
-            />
-          </div>
-        </div>
+            &
+          </em>{" "}
+          <strong
+            className="hero__strong"
+            data-aos="zoom-in"
+            data-aos-delay="500"
+            data-aos-once="true"
+          >
+            music producer
+          </strong>
+        </h1>
+      </div>
+
+      {/* Mariano photo */}
+      <div className="hero__photo hero__photo--mariano d-flex justify-content-center align-items-center vh-100 z-2">
+        <img className="hero__img-front w-50 p-5" src={mariano} alt="mariano" />
       </div>
     </section>
   );
