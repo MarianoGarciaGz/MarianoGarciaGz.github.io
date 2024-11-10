3e151a8c4e46927367da2e68f6084f6e4140b4e0
Mariano García <marianogarciagz@gmail.com>
2024-11-09 20:34:37 -0600

    🍱 Feat: Optimize posts images

--- scripts/optimize-images.js
+++ scripts/optimize-images.js
@@ -2,6 +2,7 @@ import sharp from 'sharp'
 import fs from 'fs/promises'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import os from 'os'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -19,8 +20,13 @@ async function processImagesInFolder(folderPath) {
     if (stat.isDirectory()) {
       await processImagesInFolder(filePath) // Procesa subcarpetas recursivamente
     } else if (/\.(jpe?g|png)$/i.test(file)) {
-      // Sobrescribe la imagen original ajustando solo el ancho a 500 px
-      await sharp(filePath).resize({ width: 500 }).toFile(filePath) // Mantiene la misma extensión y nombre de archivo
+      // Crea un archivo temporal para guardar la imagen redimensionada
+      const tempFilePath = path.join(os.tmpdir(), file)
+
+      await sharp(filePath).resize({ width: 500 }).toFile(tempFilePath)
+
+      // Reemplaza el archivo original con el archivo temporal
+      await fs.rename(tempFilePath, filePath)
 
       console.log(`Redimensionado: ${filePath}`)
     }
